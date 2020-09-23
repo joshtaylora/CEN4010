c763607804c5dfa57e33a459eae9523682d512d9
quid-pro-queso <alexanderianisin@gmail.com>
2020-09-22 20:39:51 -0400

    testing commit -Alex

--- pa1/src/cen4010/pa1/Vector3D.java
+++ pa1/src/cen4010/pa1/Vector3D.java
@@ -1,30 +1,28 @@
-package cen4010.pa1;
 import java.lang.*;
 
-public class Vector3D {
-	
-	private double xCoord;
-	private double yCoord;
-	private double zCoord;
+public class Vector3D{
+  private double xPt;
+  private double yPt;
+  private double zPt;
 
-	public Vector3D(double x, double y, double z) {
-		xCoord = x;
-		yCoord = y;
-		zCoord = z;
-	}
-	
-	public double dot(Vector3D v){
-		double dotP = this.xCoord * v.xCoord 
-				+ this.yCoord * v.yCoord 
-				+ this.zCoord * v.zCoord;
-		return dotP;
-	}
-	
-	public double magnitude() {
-		double mag = Math.sqrt(this.xCoord * this.xCoord 
-				+ this.yCoord * this.yCoord 
-				+ this.zCoord * this.zCoord);
-		return mag;
-	}
-	
+    public Vector3D( double x, double y, double z){
+      xPt = x;
+      yPt = y;
+      zPt = z;
+    }
+
+    public String toString(){
+      return "X coordinate: " + this.xPt + "\n" + "Y coordinate: " + this.yPt + "\n" + "Z coordinate: " + this.zPt + "\n";
+    }
+
+    public boolean equals(Vector3D vector1, Vector3D vector2){
+      if(abs(vector1.getx) == abs(vector2.getx) && abs(vector1.gety) == abs(vector2.gety) && abs(vector1.getz) == abs(vector2.getz)){
+        System.out.print("The two vectors are equal\n");
+        return true;
+      }
+      else{
+        System.out.print("The two vectors are not equal\n");
+        return false;
+      }
+    }
 }
