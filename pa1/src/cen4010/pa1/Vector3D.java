98cae54ba1387d80867c481b2f71efb6708c8467
elishawa <elishaboyette@gmail.com>
2020-09-24 12:48:07 -0400

    Commiting the edits to elisha's section of the prj

--- pa1/src/cen4010/pa1/Vector3D.java
+++ pa1/src/cen4010/pa1/Vector3D.java
@@ -1,8 +1,8 @@
 /*Current class for Vector3D until combining.*/
 public class Vector3D {
-	public double x;
-	public double y;
-	public double z;
+	private double x;
+	private double y;
+	private double z;
 	
 	public Vector3D(double x, double y, double z) {
 		this.x = x;
@@ -11,26 +11,26 @@ public class Vector3D {
 	}
 	
 	//This function is intended to take in the value of another vector and subtract it from the calling vector. Returning the new Vector
-	public Vector3D Vector3DSubtract(Vector3D sub) {
+	public Vector3D subtract(Vector3D sub) {
 		Vector3D result = new Vector3D(this.x-sub.x, this.y-sub.y, this.z-sub.z);
 		return result;
 	}
 	
 	//This function negates all of the values (i.e. multiplying each value by -1). Returning the new negated vector
-	public Vector3D Vector3DNegate() {
+	public Vector3D negate() {
 		Vector3D result = new Vector3D(this.x * -1, this.y * -1, this.z * -1);
 		return result;
 	}
 	
-	public boolean Vequals(double x1, double y1, double z1, double x2, double y2, double z2) {
-		if(x1 == x2 && y1 == y2 && z1 == z2) {
-			System.out.print("True");
-			return true;
-		}
-		else {
-			System.out.print("False");
-			return false;
-		}
-	}
+	public boolean equals(Vector3D vector1, Vector3D vector2){
+	      if(vector1.x == vector2.x && vector1.y == vector2.y && vector1.z == vector2.z){
+	        System.out.print("The two vectors are equal\n");
+	        return true;
+	      }
+	      else{
+	        System.out.print("The two vectors are not equal\n");
+	        return false;
+	      }
+	 }
 	
 }
