69223e6e59ee44a63502b7c56d8d65d06d89ef09
quid-pro-queso <alexanderianisin@gmail.com>
2020-09-24 19:46:13 -0400

    commiting changes

--- pa1/src/cen4010/pa1/Vector3D.java
+++ pa1/src/cen4010/pa1/Vector3D.java
@@ -1,27 +1,25 @@
 import java.lang.*;
 
 public class Vector3D{
-  private double xPt;
-  private double yPt;
-  private double zPt;
+  private double x;
+  private double y;
+  private double z;
 
     public Vector3D( double x, double y, double z){
-      xPt = x;
-      yPt = y;
-      zPt = z;
+      this.x = x;
+      this.y = y;
+      this.z = z;
     }
 
     public String toString(){
       return "X coordinate: " + this.xPt + "\n" + "Y coordinate: " + this.yPt + "\n" + "Z coordinate: " + this.zPt + "\n";
     }
 
-    public boolean equals(Vector3D vector1, Vector3D vector2){
-      if(abs(vector1.getx) == abs(vector2.getx) && abs(vector1.gety) == abs(vector2.gety) && abs(vector1.getz) == abs(vector2.getz)){
-        System.out.print("The two vectors are equal\n");
+    public boolean equals(Vector3D vector){
+      if(this.x == vector.x && this.y == vector.y && this.z == vector.z){
         return true;
       }
       else{
-        System.out.print("The two vectors are not equal\n");
         return false;
       }
     }
--- pa1/test/cen4010/pa1/Vector3DTest.java
+++ pa1/test/cen4010/pa1/Vector3DTest.java
@@ -5,9 +5,14 @@ class Vector3DTest{
   @Test
   void testEquals(){
     boolean equivalent;
-    Vector3D vector1 = new Vector3D(1.0, 2.0, 3.0);
-    Vector3D2 vector2 = new Vector3D(2.0, 4.0, 6.0);
-    equivalent = equals(vector1, vector2);
+    Vector3D vector = new Vector3D(1.0, 2.0, 3.0);
+    Vector3D2 vector1 = new Vector3D(2.0, 4.0, 6.0);
+    Vector3D2 vector2 = new Vector3D(1.0, 2.0, 9.0);
+    equivalent = vector.equals(vector1);
+    assertFalse(equivalent);
+    equivalent = vector.equals(vector);
+    assertTrue(equivalent);
+    equivalent = vector.equals(vector2);
     assertFalse(equivalent);
 
   }
