b7b1f204d14a9411774353fbafd9371b5ae0f270
joshtaylora <joshuataylora@gmail.com>
2020-11-08 18:05:39 -0500

    Added Timer and Token classes and began constructor for Game

--- pa3/src/Models/Game.java
+++ pa3/src/Models/Game.java
@@ -1,11 +1,28 @@
 package Models;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Game {
+	
+	// Class variables
     Player startingPlayer;
     LinkedList<Player> playerList;
+    int numPlayers;
+    int timeLimit;
     Board gameBoard;
     Dice gameDice;
+    Timer gameTimer;
+    ArrayList<Token> tokenList;
+    
+    // Class Constructor
+    Game(int numPlayers, int timeLimit, ArrayList<Token> tokenList) {
+    	// assign the number of players
+    	this.numPlayers = numPlayers;
+    	this.timeLimit = timeLimit;
+    	this.tokenList = tokenList;
+    	
+    	
+    }
 
 }
