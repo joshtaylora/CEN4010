a3233f1950a58924a5cec977779d6f6df77f30ed
joshtaylora <joshuataylora@gmail.com>
2020-11-13 17:58:41 -0500

    "pushing so that I can checkout to master and get changes from Breonna, Elisha, and Alex's branches"

--- pa3/src/Models/Game.java
+++ pa3/src/Models/Game.java
@@ -3,9 +3,20 @@ package Models;
 import java.util.LinkedList;
 
 public class Game {
-    Player startingPlayer;
-    LinkedList<Player> playerList;
-    Board gameBoard;
-    Dice gameDice;
-
+    private Player startingPlayer;
+    private LinkedList<Player> playerList;
+    private Board gameBoard;
+    private Dice gameDice;
+    private Timer gameTimer;
+    private Token[] tokenArray;
+    /**
+     * What should the main menu pass to the game class to instantiate the game object
+     * - Timer object
+     * - LinkedList of Player objects
+     * -
+     */
+    Game(LinkedList<Player> playerLinkedList, Timer gameTimer) {
+        this.playerList = playerLinkedList;
+        this.gameTimer = gameTimer;
+    }
 }
