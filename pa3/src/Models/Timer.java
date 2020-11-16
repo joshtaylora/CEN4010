d78dc52782c26bf2fbd3fc16e615628e902662f5
quid-pro-queso <alexanderianisin@gmail.com>
2020-11-16 15:57:26 -0500

    Updated Timer and Token

--- pa3/src/Models/Timer.java
+++ pa3/src/Models/Timer.java
@@ -3,12 +3,13 @@ import java.util.Calendar;
 //Timer class that has two functions to set the game timer and to check if the time limit has been reached.
 public class Timer{
   //initialize endTimer which will keep track of the time that the game will end
-  Calendar endTimer = Calendar.getInstance();
+  Calendar endTimer;
+  public Timer(int gameTime){
+    Calendar endTimer = Calendar.getInstance();
+    endTimer.add(Calendar.MINUTE, gameTime);
 
+  } 
   //Updates endTimer to be the current time plus the time limit passed into the function in minutes. No need to return  
-  public void setTimer(int gameTime){
-    endTimer = endTimer.add(Calendar.MINUTE, gameTime);
-  }
 
   //Function to check if the time limit has been reached, returns true if the time limit is reached and false otherwise. SHOULD WE CHECK SECONDS? MAKE IT EXACT TO SECONDS
   public boolean checkTime(){
@@ -27,12 +28,19 @@ public class Timer{
   }
 
   //MAKE GET TIME
-  public void getTime(){
+  public Calendar getTimer(){
+    return endTimer;
+  }
+  public int getMin(){
     Calendar currentTime = Calendar.getInstance();
-    int minLeft, secLeft, hrLeft;
-    minLeft = endTimer.get(Calendar.MINUTE) - currentTime.get(Calendar.MINUTE);
-    secLeft = endTimer.get(Calendar.SECOND) - currentTime.get(Calendar.SECONDS);
-    hrLeft = endTimer.get(Calendar.HOUR) - currentTime.get(Calendar.HOUR);
-    System.out.println("The time left in the game is: " + hrLeft +":" + minLeft +":"+ secLeft);
+    return currentTime.get(Calendar.MINUTE) - endTimer.get(Calendar.MINUTE);
+  }
+  public int getSec(){
+    Calendar currentTime = Calendar.getInstance();
+    return currentTime.get(Calendar.SECOND) - endTimer.get(Calendar.SECOND);
+  }
+  public int getHour(){
+    Calendar currentTime = Calendar.getInstance();
+    return currentTime.get(Calendar.HOUR) - endTimer.get(Calendar.HOUR);
   }
 }
--- pa3/src/Models/Token.java
+++ pa3/src/Models/Token.java
@@ -1,8 +1,25 @@
-
+//ADD X AND Y COORDINATES
 public class Token{
   string name = "";
+  int xCoor, yCoor;
 
   public int getLocation(Tile tile){
     return tile.getPosition();
+  }
+
+  public void setX(int num){
+    xCoor = num;
+  }
+
+  public int getX(){
+    return xCoor;
+  }
+
+  public void setY(int num){
+    yCoor = num;
+  }
+
+  public int getY(){
+    return yCoor;
   }
 }
