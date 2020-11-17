7e13f300567912d0779858cfaae1332118a75dd8
Elisha Boyette <elishaboyette@gmail.com>
2020-11-17 17:19:54 -0500

    Full merge for Elisha branch, not with tests yet

--- pa3/src/Models/Player.java
+++ pa3/src/Models/Player.java
@@ -58,11 +58,13 @@ public class Player {
 	 * @param cPlayerDeeds
 	 * @param tPlayerDeeds
 	 */
-	public void initiateTrade(Player tradePlayer, int cPlayerMoney, int tPlayerMoney, Deed cPlayerDeeds[], Deed tPlayerDeeds[], boolean tradeResults) {
+	public void initiateTrade(Player tradePlayer, int cPlayerMoney, int tPlayerMoney, Deed cPlayerDeeds[], Deed tPlayerDeeds[]) {
 		int i;
 		int cDeedsSize = cPlayerDeeds.length;
 		int tDeedsSize = tPlayerDeeds.length;
-
+		
+		boolean tradeResults; /* = ////tradePlayer.receiveTrade////(tradePlayer, cPlayerDeeds, tPlayerDeeds, cPlayerMoney, tPlayerMoney); TODO Josh what am I calling for the Controller? */
+		
 		if (tradeResults) {
 			account += tPlayerMoney;
 			account -= cPlayerMoney;
--- pa3/src/Models/PropertySet.java
+++ pa3/src/Models/PropertySet.java
@@ -14,8 +14,8 @@ public class PropertySet {
 	 */
 	public PropertySet(int pInSet) {
 		setSize = pInSet;
-		this.propertiesInSet = new Deed[pInSet];
-		this.currentNumProperties = 0;
+		propertiesInSet = new Deed[pInSet];
+		currentNumProperties = 0;
 	}
 	
 	/**
@@ -25,7 +25,8 @@ public class PropertySet {
 	public void addProperty(Deed property) {
 		propertiesInSet[currentNumProperties] = property;
 		currentNumProperties++;
-
+		
+		return;
 	}
 	
 	/**
@@ -69,7 +70,8 @@ public class PropertySet {
 			}
 		}
 		currentNumProperties--;
-
+		
+		return;
 	}
 	
 	/**
